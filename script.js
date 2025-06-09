82715bf849f1adf3dfaf44e6c9a5320ab589d520
rodrisaray <rodrisarayto@gmail.com>
2025-06-09 09:12:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const cta = document.querySelector('.cta');
+  cta.addEventListener('click', () => {
+    document.getElementById('connect').scrollIntoView({ behavior: 'smooth' });
+  });
+});
