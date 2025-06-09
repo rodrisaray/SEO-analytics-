11a6d1afa05bd38c762ad4994b511f13af67b579
rodrisaray <rodrisarayto@gmail.com>
2025-06-09 10:44:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,4 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-<script>
-fetch('https://yourblogname.blogspot.com/feeds/posts/default?alt=json')
-  .then(response => response.json())
-  .then(data => {
-    const posts = data.feed.entry.slice(0, 3); // Get latest 3
-    let html = '<ul>';
-    posts.forEach(post => {
-      const title = post.title.$t;
-      const url = post.link.find(l => l.rel === 'alternate').href;
-      html += `<li><a href="${url}" target="_blank">${title}</a></li>`;
-    });
-    html += '</ul>';
-    document.getElementById('blog-feed').innerHTML = html;
-  });
-</script>
+
